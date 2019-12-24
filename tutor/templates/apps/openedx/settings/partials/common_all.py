a5372e0f716cbab2312c74e46986cc1aa494751e
Régis Behmo <regis@behmo.com>
2019-12-24 17:22:12 +0100

    WIP

--- tutor/templates/apps/openedx/settings/partials/common_all.py
+++ tutor/templates/apps/openedx/settings/partials/common_all.py
@@ -1,4 +1,5 @@
 ####### Settings common to LMS and CMS
+import json
 
 DEFAULT_FROM_EMAIL = ENV_TOKENS["CONTACT_EMAIL"]
 DEFAULT_FEEDBACK_EMAIL = ENV_TOKENS["CONTACT_EMAIL"]
@@ -15,6 +16,10 @@ API_ACCESS_FROM_EMAIL = ENV_TOKENS["CONTACT_EMAIL"]
 
 # Load module store settings from config files
 update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
+DATA_DIR = "/openedx/data/fs_root"
+for store in MODULESTORE["default"]["OPTIONS"]["stores"]:
+    store["OPTIONS"]["fs_root"] = DATA_DIR
+
 
 # Set uploaded media file path
 MEDIA_ROOT = "/openedx/media/"
@@ -40,6 +45,34 @@ LOGGING["loggers"]["tracking"]["handlers"] = ["console", "local", "tracking"]
 EMAIL_USE_SSL = {{ SMTP_USE_SSL }}
 
 LOCALE_PATHS.append("/openedx/locale")
+
+{% set jwt_rsa_key = rsa_import_key(JWT_RSA_PRIVATE_KEY) %}
+JWT_AUTH["JWT_ISSUER"] = "{{ JWT_COMMON_ISSUER }}"
+JWT_AUTH["JWT_AUDIENCE"] = "{{ JWT_COMMON_AUDIENCE }}"
+JWT_AUTH["JWT_SECRET_KEY"] = "{{ JWT_COMMON_SECRET_KEY }}"
+JWT_AUTH["JWT_PRIVATE_SIGNING_JWK"] = json.dumps(
+    {
+        "kid": "openedx",
+        "kty": "RSA",
+        "e": "{{ jwt_rsa_key.e|long_to_base64 }}",
+        "d": "{{ jwt_rsa_key.d|long_to_base64 }}",
+        "n": "{{ jwt_rsa_key.n|long_to_base64 }}",
+        "p": "{{ jwt_rsa_key.p|long_to_base64 }}",
+        "q": "{{ jwt_rsa_key.q|long_to_base64 }}",
+    }
+)
+JWT_AUTH["JWT_PUBLIC_SIGNING_JWK_SET"] = json.dumps(
+    {
+        "keys": [
+            {
+                "kid": "openedx",
+                "kty": "RSA",
+                "e": "{{ jwt_rsa_key.e|long_to_base64 }}",
+                "n": "{{ jwt_rsa_key.n|long_to_base64 }}",
+            }
+        ]
+    }
+)
 
 {{ patch("openedx-common-settings") }}
 ######## End of settings common to LMS and CMS
--- tutor/utils.py
+++ tutor/utils.py
@@ -1,11 +1,14 @@
+import base64
 import json
 import os
 import random
 import shutil
 import string
+import struct
 import subprocess
 
 import click
+from Crypto.PublicKey import RSA
 
 from . import exceptions
 from . import fmt
@@ -54,6 +57,41 @@ def reverse_host(domain):
     Ex: "www.google.com" -> "com.google.www"
     """
     return ".".join(domain.split(".")[::-1])
+
+
+def rsa_private_key(bits=2048):
+    """
+    Export an RSA private key in PEM format.
+    """
+    key = RSA.generate(bits)
+    return key.export_key().decode()
+
+
+def rsa_import_key(key):
+    """
+    Import PEM-formatted RSA key and return the corresponding object.
+    """
+    return RSA.import_key(key.encode())
+
+
+def long_to_base64(n):
+    """
+    Borrowed from jwkest.__init__
+    """
+
+    def long2intarr(long_int):
+        _bytes = []
+        while long_int:
+            long_int, r = divmod(long_int, 256)
+            _bytes.insert(0, r)
+        return _bytes
+
+    bys = long2intarr(n)
+    data = struct.pack("%sB" % len(bys), *bys)
+    if not data:
+        data = "\x00"
+    s = base64.urlsafe_b64encode(data).rstrip(b"=")
+    return s.decode("ascii")
 
 
 def walk_files(path):
